f9f54f8a0b6396cfce708507d3adc11078496d70
CuckooRoachMan <cross.platform.1.0@gmail.com>
2021-02-15 07:16:39 +0000

    Add: partner model

--- openacademy/models/partner.py
+++ openacademy/models/partner.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from odoo import fields, models
 
-class Partner(models.Model):
-    #_inherit = 'res.partner'
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
 
     # Add a new column to the res.partner model, by default partners are not
     # instructors
